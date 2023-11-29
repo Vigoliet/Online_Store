d7021669e312d3cd9f48e48a128bd74a07772c11
vigoliet <vigor.lundkvist@live.se>
2023-11-29 13:01:51 +0100

    Added sample code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,26 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
-
-
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ProductManager productManager = new ProductManager();
+
+        // Create an array of Product objects
+        Product[] availableProducts = {
+                new Product("Laptop", 999.99, "Electronics"),
+                new Product("Headphones", 49.99, "Electronics"),
+                new Product("Backpack", 39.99, "Fashion"),
+                new Product("Running Shoes", 79.99, "Sportswear")
+                // Add more products as needed
+        };
+        
+        productManager.addProductToCart(availableProducts[0]);
+        productManager.printCart();
+
+
 
         Scanner scanner = new Scanner(System.in);
         UserService userService = new UserService();
@@ -23,11 +37,9 @@ public class Main {
                 case 1:
                     User newUser = UserService.getDetails();
                     userService.registerUser(newUser);
-
                     break;
                 case 2:
                     User loginUser = UserService.getDetails();
-                    //System.out.println(loginUser);
                     userService.login(loginUser);
                     break;
                 case 3:
